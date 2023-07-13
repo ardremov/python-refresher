f0e06cd58195f1b79f2f2336880ecb74d6caf7f8
ardremov <ardremov@gmail.com>
2023-07-13 10:49:31 -0400

    test physics funcs

---
+++ test_physics.py
@@ -0,0 +1,19 @@
+import unittest
+import physics
+
+class TestPhysics(unittest.TestCase):
+    def test_calc_buoyancy(self):
+        self.assertAlmostEqual(physics.calculate_buoyancy(10, 10), 981)
+        self.assertRaises(ValueError, lambda: physics.calculate_buoyancy(0, 0))
+        self.assertNotEqual(physics.calculate_buoyancy(1, 1), 10)
+
+    def test_will_it_float(self):
+        self.assertFalse(physics.will_it_float(1, 1001))
+        self.assertTrue(physics.will_it_float(1000, 1))
+        self.assertEqual(physics.will_it_float(1, 1000), None)
+        self.assertRaises(ValueError,lambda: physics.will_it_float(0, 0))
+
+    def test_calculate_pressure(self):
+        self.assertEqual(physics.calculate_pressure(0), 101.3)
+        self.assertNotEqual(physics.calculate_pressure(100), 101.3)
+        self.assertEqual(physics.calculate_pressure(-100), 981000)
