2b22ecc9b4e48e743bbb37415dc006786d61fd14
ardremov <ardremov@gmail.com>
2023-07-14 11:59:32 -0400

    physics.py problems 4 - 8a completed

--- physics.py
+++ physics.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 g = 9.81 # m/s^2
 d_water = 1000 # density water
 
 '''
+Problem #1
 buoyancy force = density fluid * volume * gravitational accel constant
 Given volume and density of fluid, will return the buoyancy force in Newtons.
 '''
@@ -10,7 +13,10 @@ def calculate_buoyancy(V, density_fluid):
         raise ValueError("Cannot calculate buoyancy of an object with no volume or density.")
     return g * V * density_fluid
 
-'''Given volume and mass, will return true if the object can float or false if it sinks.'''
+'''
+Problem #2
+Given volume and mass, will return true if the object can float or false if it sinks.
+'''
 def will_it_float(V, mass):
     if V <= 0 or mass <= 0:
         raise ValueError("Cannot calculate buoyancy of an object with no volume or mass.")
@@ -20,6 +26,7 @@ def will_it_float(V, mass):
     return den < d_water
 
 '''
+Problem #3
 pressure = density fluid * gravitational accel constant * depth
 Given depth (negative or positive, it runs through abs), calculates the pressure in pascals and returns it.
 '''
@@ -30,3 +37,98 @@ def calculate_pressure(depth):
     elif depth < 0:
         depth = abs(depth)
     return pressure + d_water * g * depth
+
+'''
+Problem #4
+Provided a force in newtons and a mass in kg, this function will compute acceleration.
+It raises a value error in the case that mass is less than or equal to 0.
+'''
+def calculate_acceleration(F, m):
+    if m <= 0:
+        raise ValueError("Cannot calculate acceleration of an object with no mass.")
+    return F / m
+
+'''
+Problem #5
+Provided a torque in newton-meters and a moment of inertia in m^2kg, this function will compute angular acceleration.
+It raises a value error in the case that the moment of inertia is less than or equal to 0.
+'''
+def calculate_angular_acceleration(tau, I):
+    if I <= 0:
+        raise ValueError("Cannot calculate angular acceleration of an object with no inertia.")
+    return tau / I
+
+'''
+Problem #6
+Given the force magnitude in newtons, the force direction in degrees, and the radius in meters,
+this function will return torque in newton-meters.
+'''
+def calculate_torque(F_magnitude, F_direction, r):
+    return r * F_magnitude * np.sin(F_direction * np.pi / 180)
+
+'''
+Problem #7
+Given the mass in kg and the radius in meters,
+this function will return I, the moment of inertia in m^2kg.
+'''
+def calculate_moment_of_inertia(m, r):
+    return m * r * r
+
+'''
+(needed for problem 8)
+supplemental methods that calculate rotation matrix and rov rotation matrix given respective angles
+'''
+def get_rotation_matrix(theta):
+    rot_mat = np.array(
+        [
+            [np.cos(theta), -np.sin(theta)],
+            [np.sin(theta), np.cos(theta)]
+        ]
+    )
+    return rot_mat
+
+def get_rov_rotation_matrix(a):
+    rov_rot_mat = np.array(
+        [
+            [np.cos(a), np.cos(a), -np.cos(a), -np.cos(a)],
+            [np.sin(a), -np.sin(a), -np.sin(a), np.sin(a)]
+        ]
+    )
+    return rov_rot_mat
+
+'''
+Problem #8
+
+a)
+Given:
+F_magnitude: the magnitude of force applied by the thruster in Newtons.
+F_angle: the angle of the force applied by the thruster in radians. The angle is measured from the x axis. Positive angles are measured in the counter-clockwise direction.
+mass (optional): the mass of the AUV in kilograms. The default value is 100kg. 
+volume (optional): the volume of the AUV in cubic meters. The default value is 0.1m^3
+thruster_distance(optional): the distance from the center of mass of the AUV to the thruster in meters. The default value is 0.5m.
+The function returns the acceleration of the AUV in meters per second squared.
+'''
+def calculate_auv_acceleration(F_magnitude, F_angle, mass = 100, volume = 0.1, thruster_distance = 0.5):
+    rov_rot_mat = get_rov_rotation_matrix(F_angle) # alpha
+    thrusters = np.array([[F_magnitude], [F_magnitude], [F_magnitude], [F_magnitude]])
+    force_matrix = np.matmul(rov_rot_mat, thrusters)
+    a = force_matrix / mass
+    return a
+
+'''
+Problem #8
+
+b)
+Given:
+F_magnitude: the magnitude of force applied by the thruster in Newtons.
+F_angle: the angle of the force applied by the thruster in radians. The angle is measured from the x axis. Positive angles are measured in the counter-clockwise direction.
+inertia (optional): the moment of inertia of the AUV in kgm^2. The default value is 1 kgm^2.
+thruster_distance(optional): the distance from the center of mass of the AUV to the thruster in meters. The default value is 0.5m.
+The function returns the angular acceleration of the AUV in radians per second squared.
+'''
+#def calculate_auv_angular_acceleration():
+
+
+'''
+Problem #9
+'''
